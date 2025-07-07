068fdfc5fe44d60a2bbb6b08e510da9583013f36
mayank8630 <mayank.singh_cs23@gla.ac.in>
2025-07-07 12:47:09 +0530

    server

--- programFile/server.js
+++ programFile/server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+js
+const path = require("path");
+app.use(express.static(path.join(__dirname)));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+js
+const fs = require("fs");
+const usersFile = path.join(__dirname, "data", "users.json");
+const votesFile = path.join(__dirname, "data", "votes.json");
 
 app.use(cors());
 app.use(express.json());
