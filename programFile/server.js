084e065757afef786ffb1ed1116232001a7fd781
mayank8630 <mayank.singh_cs23@gla.ac.in>
2025-07-15 12:04:16 +0530

    server

--- programFile/server.js
+++ programFile/server.js
@@ -1,37 +1,16 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const path = require('path');
 const app = express();
-js
-const path = require("path");
-app.use(express.static(path.join(__dirname)));
+const authRoutes = require('./routes/auth');
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-js
-const fs = require("fs");
-const usersFile = path.join(__dirname, "data", "users.json");
-const votesFile = path.join(__dirname, "data", "votes.json");
-js
-app.post("/api/signup", (req, res) => {
-    const { name, email, password } = req.body;
-
-    const users = JSON.parse(fs.readFileSync(usersFile)).users;
-    if (users.find(u => u.email === email)) {
-        return res.status(400).json({ message: "User already exists" });
-    }
-
-    const newUser = { id: Date.now(), name, email, password };
-    users.push(newUser);
-    fs.writeFileSync(usersFile, JSON.stringify({ users }, null, 2));
-
-    res.json({ message: "User registered!" });
-});
-
-
-app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(5000, () => {
-    console.log("Server running on http://localhost:5000");
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+app.use('/api/auth', authRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${5000}`);
 });
