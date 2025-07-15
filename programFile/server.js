b6d9055e5fb73f3f281d728d84a9a4934005972e
mayank8630 <mayank.singh_cs23@gla.ac.in>
2025-07-15 10:17:14 +0530

    store

--- programFile/server.js
+++ programFile/server.js
@@ -12,6 +12,22 @@ js
 const fs = require("fs");
 const usersFile = path.join(__dirname, "data", "users.json");
 const votesFile = path.join(__dirname, "data", "votes.json");
+js
+app.post("/api/signup", (req, res) => {
+    const { name, email, password } = req.body;
+
+    const users = JSON.parse(fs.readFileSync(usersFile)).users;
+    if (users.find(u => u.email === email)) {
+        return res.status(400).json({ message: "User already exists" });
+    }
+
+    const newUser = { id: Date.now(), name, email, password };
+    users.push(newUser);
+    fs.writeFileSync(usersFile, JSON.stringify({ users }, null, 2));
+
+    res.json({ message: "User registered!" });
+});
+
 
 app.use(cors());
 app.use(express.json());
